d3f329ae344d8baa7196162b399717f21fe262e6
Vinny Tacaná <vini.tacana@gmail.com>
2025-12-09 17:52:35 -0400

    fix(ui): update frontend to support new chemical types

--- main.py
+++ main.py
@@ -23,11 +23,9 @@ app.include_router(energia.router, prefix="/energia", tags=["Energia"])
 app.include_router(relatorios.router, prefix="/relatorios", tags=["Relatórios"])
 app.include_router(residuos.router, prefix="/residuos", tags=["Resíduos"])
 
-# --- NOVO: SERVIR O FRONT-END ---
-# Isso faz com que seus arquivos HTML fiquem acessíveis
+
 app.mount("/app", StaticFiles(directory="static", html=True), name="static")
 
-# Redirecionar a raiz (/) para o index.html
 @app.get("/")
 def home():
     return RedirectResponse(url="/app/index.html")
