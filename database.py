368ac97b483107805f718556ed43ecb251f9aff7
Vinicius Tacana <vini.tacana@gmail.com>
2026-02-18 22:35:35 -0400

    refactor(database): update SQLAlchemy declarative base imports for consistency

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 # database.py
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base # Keeping import but updating usage if needed or just changing import
+from sqlalchemy.orm import sessionmaker, declarative_base
 from config import settings
 
 engine = create_engine(settings.DB_URL, connect_args={"check_same_thread": False})
