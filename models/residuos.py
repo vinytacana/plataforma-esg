0c662497adf9dc086fa98baea92fcb4e64016ced
Vinicius Tacana <vini.tacana@gmail.com>
2026-02-18 22:38:48 -0400

    feat(models): add tenant_id to ResiduoModel for tenant data isolation

--- models/residuos.py
+++ models/residuos.py
@@ -8,6 +8,7 @@ class ResiduoModel(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     data_calculo = Column(DateTime, default=datetime.utcnow)
+    tenant_id = Column(String, index=True, nullable=False)
     
     tipo = Column(String)
     destino = Column(String)
